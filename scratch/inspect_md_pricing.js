559a1df931774a2e2a2c7b6b143a619af5904c5b
QuangZo <taducquang@gmail.com>
2026-06-19 17:35:10 +0700

    Clean up old scratch scripts in repository

--- scratch/inspect_md_pricing.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-const path = require('path');
-
-const filePath = path.join(__dirname, '..', '03_bao_gia_va_specs_chi_tiet.md');
-const content = fs.readFileSync(filePath, 'utf8');
-
-console.log('=== FIRST 100 LINES OF 03_bao_gia_va_specs_chi_tiet.md ===');
-console.log(content.split('\n').slice(0, 100).join('\n'));
